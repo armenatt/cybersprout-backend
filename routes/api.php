0fb750c1f34763f25ebdf1aef63f8b29fd58bb4b
constab1e <armenat3@gmail.com>
2021-09-16 21:37:36 +0900

    added auth/user route

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,5 +19,5 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('auth')->group(function () {
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
-
+    Route::get('/user', [UserController::class, 'user'])->middleware('auth:api');
 });
